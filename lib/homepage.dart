c2706d410bc93bd4afbf90a77a4d039783f2e696
itsretroboi <saswatkarma@gmail.com>
2021-07-22 04:18:12 +0530

    Update homepage.dart

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -36,7 +36,7 @@ class _HomeState extends State<Home> {
                         child: Text(
                           "Hey $name",
                           style: TextStyle(
-                              color: Colors.red,
+                              color: Colors.white,
                               fontSize: 35,
                               fontWeight: FontWeight.bold),
                         ),
@@ -44,10 +44,36 @@ class _HomeState extends State<Home> {
                 ],
               ),
             ),
-            Container(
-              height: 250,
-              width: 250,
-              color: Colors.amber,
+            Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(left: 8, right: 8, top: 25),
+                  height: 75,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 8, right: 8, top: 25),
+                  height: 75,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 8, right: 8, top: 25),
+                  height: 75,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
